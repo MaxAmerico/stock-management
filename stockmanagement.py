4bbe3bb90d27fa8418cff21fa7adeee0ddb99d95
MaxAmerico <maxdarosa561@gmail.com>
2026-03-11 13:50:41 -0300

    fix: tables

--- stockmanagement.py
+++ stockmanagement.py
@@ -1,5 +1,14 @@
+import sqlite3
+
 print('- - - Welcome to the stock management - - -')
 print('- - - Please, sign in or sign up - - -')
+
+conection = sqlite3.conection('users.db')
+cursor = conection.cursor()
+
+cursor.execute
+
+
 ##Login area
 #(Soon)
 ##############
