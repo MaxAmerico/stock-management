7bfa2a8802e58cd5f60b731e17f2b5838ce9e901
MaxAmerico <maxdarosa561@gmail.com>
2026-04-02 11:38:02 -0300

    trying to add soem things else

--- stockmanagement.py
+++ stockmanagement.py
@@ -15,16 +15,32 @@ with open('users.json', 'w') as file:
 print('- - - Welcome to stock menagement - - -')
 
 login_or_register = input('What would you wish? A. Login. B. Register:').upper()
-
-if login_or_register == 'A':
- username = input('Put your username here:')
- password = input('Put your password here:')
+while True:
+ if login_or_register == 'A':
+  username = input('Put your username here:')
+  password = input('Put your password here:')
 
  if (username in dic_accounts and dic_accounts[username] == password):
+         
          def main_code():
            randomnumber = random.randint(1,999)
-           print('Name: {username} ')
+           print(f'Name: {username}')
            print(f'Temporary user code: {randomnumber}')
+         break
+
+
+ else:
+     count = 0
+     print('Please, try again(maximum 3 times)')  
+     count =+ 1 
+     if count == 3:
+        print('Maximum attempts, please, restart the program')  
+        break
+     else: 
+      continue  
+           
+
+
 
 
 
