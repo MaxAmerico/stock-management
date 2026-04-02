8e9a1932114a2c14a2f70d7b0fc81bed33aae4c1
MaxAmerico <maxdarosa561@gmail.com>
2026-04-01 23:09:10 -0300

    Changing the code

--- stockmanagement.py
+++ stockmanagement.py
@@ -1,21 +1,30 @@
-import sqlite3
+import json
+import random
+dic_accounts = {'joao':'ehwh8',
+        'maria':'eneee2',
+        'pedro':'rn3r3',
+        'jose':'fferfge5'
+                        }                    
 
-print('- - - Welcome to the stock management - - -')
-print('- - - Please, sign in or sign up - - -')
+file_json = json.dumps(dic_accounts, indent=4)
 
-conection = sqlite3.conection('users.db')
-cursor = conection.cursor()
+with open('users.json', 'w') as file:
+    file.write(file_json)
+ 
+#Start code
+print('- - - Welcome to stock menagement - - -')
 
-cursor.execute
+login_or_register = input('What would you wish? A. Login. B. Register:').upper()
 
+if login_or_register == 'A':
+ username = input('Put your username here:')
+ password = input('Put your password here:')
 
-##Login area
-#(Soon)
-##############
-
-#Functions
-
-
+ if (username in dic_accounts and dic_accounts[username] == password):
+         def main_code():
+           randomnumber = random.randint(1,999)
+           print('Name: {username} ')
+           print(f'Temporary user code: {randomnumber}')
 
 
 
